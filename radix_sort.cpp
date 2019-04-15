a76c515a20f173c671da886c1540c4a429085f63
cat of Mr Lu <lxxlxx8888@outlook.com>
2019-04-15 22:55:37 +0800

    update

---
+++ radix_sort.cpp
@@ -0,0 +1,104 @@
+# include "iostream"
+
+using namespace std;
+
+// 使用基数排序算法排序
+
+// 该算法尚未完成！！
+
+int main(){
+
+    int input_data[20];
+
+    // 输入部分
+    {
+        // for(int i = 0; i < 20; i++)
+        // {
+        //     /* code */
+        //     try
+        //     {
+        //         cout << "please input the value of  " << i << endl;
+        //         cin >> input_data[i];
+        //         /* code */
+        //     }
+        //     catch(const std::exception& e)
+        //     {
+        //         std::cerr << e.what() << '\n';
+        //     }catch(io_errc e){
+        //         cout << e;
+        //     }
+        // }
+
+        int input_data[20] = 
+        {32, 2, 43, 0, 43, 
+        11, 48, 983, 56, 43, 
+        11, 0, 87, 66, 42, 
+        31, 22, 20, 98, 35};
+        
+        cout << endl << "input complete!" << endl;
+        cout << "input result:";
+
+        for(int i = 0; i < 20; i++)
+            cout << input_data[i] << ", ";
+            
+        cout << endl;
+    }
+    
+    // 算法部分
+    int value_max = input_data[0];
+    for (int i = 1; i < 20; i++)
+        if (input_data[i] > value_max)
+            value_max = input_data[i];
+
+    int d = 0;
+    while (value_max > 0)
+    {
+        d++;
+        value_max /= 10;
+    }
+
+    int k, j, count[10], temp[20], radix = 1;
+    for (int i = 0; i < d; i++)
+    {
+        /* code */
+        for ( i = 0; i < 10; i++)
+            count[i] = 0;
+        for ( i = 0; i < 20; i++)
+        {
+            k = input_data[i] / radix % 10;
+            count[k]++; /*计数器*/
+        }
+
+        for ( j = 1; j < 10; j++)
+            count[j] = count[j - 1] + count[j];
+        for (j = 19; j >= 0; j--)
+        {
+            /* code */
+            k = (input_data[j] / radix) % d;
+            temp[count[k] - 1] = input_data[j];
+            count[k]--;
+        }
+
+        cout << "into last for" << endl;
+
+        for (j = 0; i < 20; j++)
+            input_data[j] = temp[j];
+
+        radix *= 10;
+        
+    }
+    
+    
+    // 输出部分
+    {
+        cout << "sort completed!" << endl;
+        cout << "sorted result:";
+
+        for(int i = 0; i < 20; i++)
+        {
+            cout << input_data[i] << ",";
+        }
+    }
+
+    return 0;
+}
